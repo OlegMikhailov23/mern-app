be03cb2031e810623e437d7cf8b61f57ddb39fc5
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-02-20 15:14:30 +0300

    feat: added auth

---
+++ routes/auth.routes.js
@@ -0,0 +1,95 @@
+const {Router} = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator');
+const User = require('../models/User');
+const router = Router();
+
+// /api/auth/register
+router.post(
+    '/register',
+    [
+        check('email', 'Bad email address').isEmail(),
+        check('password', 'Password should be min. 6 characters ')
+            .isLength({min: 6})
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Invalid data!'
+                })
+            }
+
+            const {email, password} = req.body;
+            const candidate = await User.findOne({email});
+
+            if (candidate) {
+                return res.status(400).json({message: 'This user has already exist!'})
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const user = new User({email, password: hashedPassword})
+
+            await user.save()
+
+            res.status(201).json({message: 'User has been created'});
+
+        } catch (e) {
+            res.status(500).json({message: 'Oops, something goes wrong'});
+        }
+    })
+
+// /api/auth/login
+
+router.post(
+    '/login',
+    [
+        check('email', 'Please enter a valid email').normalizeEmail().isEmail(),
+        check('password', 'Please enter your password').exists()
+            .isLength({min: 6})
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Invalid login data!'
+                })
+            }
+
+            const {email, password} = req.body;
+
+            const user = await User.findOne({email})
+
+            if (!user) {
+                return res.status(400).json({message: 'User is not found'})
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch) {
+                return res.status(400).json({message: 'Invalid password, try again'})
+            }
+
+            const token = jwt.sign(
+                {userId: user.id},
+                config.get('jwtSecret'),
+                { expiresIn: '1h' }
+            );
+
+            res.json({ token, userId: user.id })
+
+
+        } catch (e) {
+            res.status(500).json({message: 'Oops, something goes wrong'});
+        }
+    })
+
+module.exports = router
