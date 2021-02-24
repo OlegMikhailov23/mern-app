84d14a4befc933c0cffb1b912046c38ceb4e92a9
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-02-24 20:02:24 +0300

    feat: added Link List

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import {BrowserRouter as Router} from "react-router-dom"
 import useRoutes from "./routes"
 import useAuth from "./hooks/auth.hook"
 import AuthContext from "./context/AuthContext"
-import Navbar from "./conponents/Navbar"
+import Navbar from "./components/Navbar"
 import 'materialize-css'
-import Loader from "./conponents/Loader";
+import Loader from "./components/Loader";
 
 
 function App() {
